c2a231dff5545074d7dde1f250e9fcb3046b53e0
Oleg Vrabiy <olegvraby@gmail.com>
2025-03-01 19:48:49 +0200

    update:App.tsx: navbar component has been added

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { AppRoutes } from "./routes/app-routes";
+import { Navbar } from "@/components/navbar";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <AppRoutes />
       </Router>
     </>
