ae9f64b3819e60660027319526ca1c37dd682a5d
Oleg Vrabiy <olegvraby@gmail.com>
2025-03-02 14:36:57 +0200

    create:loading.tsx

---
+++ client/src/app/pages/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader } from "lucide-react";
+
+const LoadingPage = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Loader className="text-muted-foreground size-6 animate-spin" />
+    </div>
+  );
+};
+
+export default LoadingPage;
