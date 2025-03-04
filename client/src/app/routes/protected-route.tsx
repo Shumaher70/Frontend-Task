e562bfe036285e20a29eac7a3a78f3869cf3639d
Oleg Vrabiy <olegvraby@gmail.com>
2025-03-04 14:24:03 +0200

    update:protected-route.tsx: An additional check if user explicit remove data from "localStorage"

--- client/src/app/routes/protected-route.tsx
+++ client/src/app/routes/protected-route.tsx
@@ -1,9 +1,16 @@
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
 import { useAuthStore } from "@/features/constants";
 
 const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
+  useEffect(() => {
+    const storedData = localStorage.getItem("auth-storage");
+    if (!storedData) {
+      useAuthStore.getState().logout();
+    }
+  }, []);
+
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   return isAuthenticated ? element : <Navigate to="/login" />;
 };
